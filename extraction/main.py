7f0395188f5758893283c6571b7d4a8427568564
Israel Eze <ezeisraeljohn@gmail.com>
2025-06-16 17:46:40 +0100

    Refactor main.py to enhance person detection logic and streamline message handling

--- extraction/main.py
+++ extraction/main.py
@@ -3,14 +3,21 @@ from pydantic import BaseModel, Field
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain.chat_models import init_chat_model
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_ollama import ChatOllama
 import os
 import getpass
 from typing import List
+from langchain_core.utils.function_calling import tool_example_to_messages
 
 if not os.environ.get("GOOGLE_API_KEY"):
     os.environ["GOOGLE_API_KEY"] = getpass.getpass("Enter GOOGLE API KEY")
 
 llm = init_chat_model("gemini-2.0-flash", model_provider="google_genai")
+# llm2 = ChatOllama(
+#     model="llama3.1",
+#     temperature=0,
+#     # other params...
+# )
 
 
 class Person(BaseModel):
@@ -58,9 +65,42 @@ prompt_template = ChatPromptTemplate(
     ]
 )
 
-structured_llm = llm.with_structured_output(schema=Data)
+# structured_llm = llm.with_structured_output(schema=Data)
 
-text = "My name is Jeff, my hair is black and i am 6 feet tall. Anna has the same color hair as me and height."
-prompt = prompt_template.invoke({"text": text})
-response = structured_llm.invoke(prompt)
+# text = "My name is Jeff, my hair is black and i am 6 feet tall. Anna has the same color hair as me and height."
+# prompt = prompt_template.invoke({"text": text})
+# response = structured_llm.invoke(prompt)
+# print(response)
+
+examples = [
+    (
+        "The ocean is vast and blue. It's more than 20,000 feet deep",
+        Data(people=[]),
+    ),
+    (
+        "Fiona traveled far from france to Spain",
+        Data(people=[Person(name="Fiona", height_in_meters=None, hair_color=None)]),
+    ),
+]
+
+messages = []
+
+for txt, tool_call in examples:
+    if tool_call.people:
+        # This final message is optional for some providers
+        ai_response = "Detected people"
+    else:
+        ai_response = "Detected no people"
+
+    messages.extend(tool_example_to_messages(txt, [tool_call], ai_response=ai_response))
+
+# for message in messages:
+#     message.pretty_print()
+
+message_no_extraction = {
+    "role": "user",
+    "content": "The solar system is large, but earth has only 1 moon.",
+}
+structured_llm = llm.with_structured_output(schema=Data)
+response = structured_llm.invoke(messages + [message_no_extraction])
 print(response)
