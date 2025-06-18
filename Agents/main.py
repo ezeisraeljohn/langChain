21d0e3c690644e3870a1a3fe7bfa683f2c136d2e
Israel Eze <ezeisraeljohn@gmail.com>
2025-06-18 14:57:04 +0100

    Refactor main.py to switch from model-based to ChatOllama implementation and enhance agent execution with memory management

--- Agents/main.py
+++ Agents/main.py
@@ -1,6 +1,7 @@
 import getpass
 import os
 from dotenv import load_dotenv
+from pprint import pprint
 
 load_dotenv()
 
@@ -10,21 +11,31 @@ from langchain_community.tools.tavily_search import TavilySearchResults
 from langchain.chat_models import init_chat_model
 from langchain_core.messages import HumanMessage
 from langgraph.prebuilt import create_react_agent
+from langgraph.checkpoint.memory import MemorySaver
+from langchain_ollama import ChatOllama
 
-model = init_chat_model("gemini-2.0-flash", model_provider="google-genai")
+# model = init_chat_model("gemini-2.0-flash", model_provider="google-genai")
+llm = ChatOllama(
+    model="llama3.1",
+    temperature=0,
+)
 
 search = TavilySearchResults(max_results=2)
 tools = [search]
-models_with_tools = model.bind_tools(tools)
+models_with_tools = llm.bind_tools(tools)
 
+memory = MemorySaver()
+agent_executor = create_react_agent(llm, tools, checkpointer=memory)
 
-agent_executor = create_react_agent(model, tools)
+config = {"configurable": {"thread_id": "abc123"}}
 
-# response = agent_executor.invoke(
-#     {"messages": [HumanMessage(content="whats the weather in sf?")]}
-# )
+response = agent_executor.invoke(
+    {"messages": [HumanMessage(content="What is the weather in sf")]}, config=config
+)
+pprint(response)
 
-for step in agent_executor.stream(
-    {"messages": [HumanMessage(content="What is the weather in sf?")]}
-):
-    step["messages"][-1].pretty_print()
+# for step in agent_executor.stream(
+#     {"messages": [HumanMessage(content="What is the weather in sf?")]},
+#     stream_mode="messages",
+# ):
+#     print(step)
